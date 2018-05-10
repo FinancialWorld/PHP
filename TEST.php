7baa6df2d0b46f779b95a2e842f8e968a6e1b907
FinancialWorld <33783502+FinancialWorld@users.noreply.github.com>
2018-05-10 11:20:41 +0200

    Update TEST.php

--- TEST.php
+++ TEST.php
@@ -1,24 +1,9 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-  	<title>Bonds</title>
-  <meta http-equiv="Content-Type" content="text/html"/>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1"/> <!-- To ensure proper rendering and touch zooming -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> <!-- Obligatoire pour importer la mise en forme du menu  -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> <!-- For the drop down menu  -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> <!-- For the drop down menu  -->
-
-</head>
-
-<body>
-  </p>
-    hello
-  <p>
-</body>
-
-<footer>
-</footer>
-
-</html>
+<?php
+echo "<p>AJAX = Asynchronous JavaScript and XML</p>";
+echo "<p>CSS = Cascading Style Sheets</p>";
+echo "<p>HTML = Hyper Text Markup Language</p>";
+echo "<p>PHP = PHP Hypertext Preprocessor</p>";
+echo "<p>SQL = Structured Query Language</p>";
+echo "<p>SVG = Scalable Vector Graphics</p>";
+echo "<p>XML = EXtensible Markup Language</p>";
+?>
