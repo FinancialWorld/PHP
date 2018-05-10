c70749b21e02d405987472776b43cf97f99bc50f
FinancialWorld <33783502+FinancialWorld@users.noreply.github.com>
2018-05-10 10:51:03 +0200

    Update and rename .gitignore to test.php

---
+++ .gitignore/test.php
@@ -0,0 +1,9 @@
+<?php
+echo "<p>AJAX = Asynchronous JavaScript and XML</p>";
+echo "<p>CSS = Cascading Style Sheets</p>";
+echo "<p>HTML = Hyper Text Markup Language</p>";
+echo "<p>PHP = PHP Hypertext Preprocessor</p>";
+echo "<p>SQL = Structured Query Language</p>";
+echo "<p>SVG = Scalable Vector Graphics</p>";
+echo "<p>XML = EXtensible Markup Language</p>";
+?>
